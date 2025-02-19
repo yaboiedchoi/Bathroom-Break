7371541b8fd906c209e4fce171a4d998455a776d
Nathan McAndrew (RIT Student) <nvm7252@rit.edu>
2025-02-19 15:58:55 -0500

    Created new scene for testing agent behavior

---
+++ SP2025GJ/GameJam2025/Assets/Scripts/AgentBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentBehavior : MonoBehaviour
+{
+    [SerializeField]Transform playerTransform; //destination of agent
+    [SerializeField]NavMeshAgent agent; //agent component
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTransform.position);      
+    }
+}
